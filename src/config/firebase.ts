3c4161460d00198f2cd48cfb2535f6082a9dc45c
Nathan985 <nathan.rodrigu3s@gmail.com>
2023-06-15 21:44:21 -0300

    feat(Auth): add connection with firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2DrvXq8Xvuh1Cvz5YsV1P9bLUdagXoaA',
+  authDomain: 'zeta-matrix-357017.firebaseapp.com',
+  projectId: 'zeta-matrix-357017',
+  storageBucket: 'zeta-matrix-357017.appspot.com',
+  messagingSenderId: '383060531729',
+  appId: '1:383060531729:web:ab2ed8385cd5b7880c6fb0',
+  measurementId: 'G-J320FF0PT8',
+};
+
+export const app = initializeApp(firebaseConfig);

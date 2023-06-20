d5a4c3f1dca0149cf12814e66ac3cb4ad5a0a3d2
Nathan985 <nathan.rodrigu3s@gmail.com>
2023-06-20 18:09:42 -0300

    feat(rest): $ user rest config

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,6 +8,7 @@ export const TypeOrmConfig = () =>
     password: process.env.PASS_DB,
     username: process.env.USER_DB,
     port: Number(process.env.PORT_DB),
+    ssl: true,
     synchronize: true,
     entities: ['dist/entities/*.entity.js'],
   } as TypeOrmModuleOptions);
---
+++ src/user/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/entities/User.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private _userRepository: Repository<UserEntity>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const isEmailExists = await this._userRepository.findBy({
+      email: createUserDto.email,
+    });
+
+    if (isEmailExists) {
+      throw new BadRequestException('email already exists!');
+    }
+
+    return await this._userRepository.save(createUserDto);
+  }
+
+  async findAll() {
+    return await this._userRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this._userRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const isEmailExists = await this._userRepository.findBy({
+      email: updateUserDto.email,
+    });
+
+    if (isEmailExists) {
+      throw new BadRequestException('email already exists!');
+    }
+
+    await this._userRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+}
